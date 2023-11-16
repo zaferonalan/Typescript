c5788dc429ef3e4d6c1f1a9648c282d7786557e7
zafergunayonalan <zafergunay7@gmail.com>
2023-11-16 18:08:31 +0300

    For

--- 2-Ders-Donguler/app.js
+++ 2-Ders-Donguler/app.js
@@ -15,30 +15,43 @@
 //   ? console.log("X Y den büyüktür")
 //   : console.log("X Y den küçük veya eşittir");
 // !Switch Case
-var day = 20;
-switch (day) {
-    case 0:
-        console.log("Pazar");
-        break;
-    case 1:
-        console.log("Pazartesi");
-        break;
-    case 2:
-        console.log("Salı");
-        break;
-    case 3:
-        console.log("Çarşamba");
-        break;
-    case 4:
-        console.log("Perşembe");
-        break;
-    case 5:
-        console.log("Cuma");
-        break;
-    case 6:
-        console.log("Cumartesi");
-        break;
-    default:
-        console.log("Böyle bir gün yok");
-        break;
-}
+// let day: number = 20;
+// switch (day) {
+//   case 0:
+//     console.log("Pazar");
+//     break;
+//   case 1:
+//     console.log("Pazartesi");
+//     break;
+//   case 2:
+//     console.log("Salı");
+//     break;
+//   case 3:
+//     console.log("Çarşamba");
+//     break;
+//   case 4:
+//     console.log("Perşembe");
+//     break;
+//   case 5:
+//     console.log("Cuma");
+//     break;
+//   case 6:
+//     console.log("Cumartesi");
+//     break;
+//   default:
+//     console.log("Böyle bir gün yok");
+//     break;
+// }
+// ! For
+// for (let i = 0; i < 3; i++) {
+//   console.log("i degerim", i);
+// }
+// let arr = [13, 27, 32, 54];
+// for (let item of arr) {
+//   console.log(item);
+// }
+// console.log(" ");
+// ? eğer index numarasını istiyorsa
+// for (let item in arr) {
+//   console.log(item);
+// }
