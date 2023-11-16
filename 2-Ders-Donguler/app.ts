2500c7216fab37e25b040225b15cb00f957c4dee
zafergunayonalan <zafergunay7@gmail.com>
2023-11-16 17:53:53 +0300

    if-else

---
+++ 2-Ders-Donguler/app.ts
@@ -0,0 +1,21 @@
+// npm install --save-dev lite-server
+// tsc app.ts --w
+
+// ! İf-else
+
+// let x: number = 55;
+// let y: number = 25;
+
+// if (x > y) {
+//   console.log("X Y'den büyüktür");
+// } else if (x < y) {
+//   console.log("X Y'den küçüktür");
+// } else {
+//   console.log("X ve Y eşittir");
+// }
+
+// * TEK satır yazımı
+
+// x > y
+//   ? console.log("X Y den büyüktür")
+//   : console.log("X Y den küçük veya eşittir");
