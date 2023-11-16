5663996b6cc7f8de4ce963278c71b138d684e346
zafergunayonalan <zafergunay7@gmail.com>
2023-11-16 17:57:56 +0300

    Switch Case

--- 2-Ders-Donguler/app.ts
+++ 2-Ders-Donguler/app.ts
@@ -19,3 +19,34 @@
 // x > y
 //   ? console.log("X Y den büyüktür")
 //   : console.log("X Y den küçük veya eşittir");
+
+// !Switch Case
+
+let day: number = 20;
+
+switch (day) {
+  case 0:
+    console.log("Pazar");
+    break;
+  case 1:
+    console.log("Pazartesi");
+    break;
+  case 2:
+    console.log("Salı");
+    break;
+  case 3:
+    console.log("Çarşamba");
+    break;
+  case 4:
+    console.log("Perşembe");
+    break;
+  case 5:
+    console.log("Cuma");
+    break;
+  case 6:
+    console.log("Cumartesi");
+    break;
+  default:
+    console.log("Böyle bir gün yok");
+    break;
+}
