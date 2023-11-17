f23851219270ad812ccf465a49bfe186e0d75ee8
zafergunayonalan <zafergunay7@gmail.com>
2023-11-17 19:59:08 +0300

    Public,Privite,Protected

--- 3-Ders-Class-Interface/app.js
+++ 3-Ders-Class-Interface/app.js
@@ -1,18 +1,24 @@
 // ! Class
-var Person = /** @class */ (function () {
-    /**
-     *
-     */
-    function Person(_id, _firsName, _lastName) {
-        this.id = _id;
-        this.firstName = _firsName;
-        this.lastName = _lastName;
-    }
-    Person.prototype.getFullname = function () {
-        return "".concat(this.firstName, " ").concat(this.lastName);
-    };
-    return Person;
-}());
-var kisibilgi = new Person(20, "Zafer", "Önalan");
-console.log(kisibilgi);
-console.log(kisibilgi.getFullname());
+// class Person {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+//   /**
+//    *
+//    */
+//   constructor(_id: number, _firsName: string, _lastName: string) {
+//     this.id = _id;
+//     this.firstName = _firsName;
+//     this.lastName = _lastName;
+//   }
+//   getFullname() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+// let kisibilgi = new Person(20, "Zafer", "Önalan");
+// console.log(kisibilgi);
+// console.log(kisibilgi.getFullname());
+// ! Public,Privite,Protected
+// ? Public default olarak geliyor. Her yerden erişimi var.
+// ? Protected ise erişimi sadece oluşturlan classı türetildiği yerde erişimi var.
+// ? Private ise erişi hiç bir yerden yok
--- 3-Ders-Class-Interface/app.ts
+++ 3-Ders-Class-Interface/app.ts
@@ -1,24 +1,29 @@
 // ! Class
 
-class Person {
-  id: number;
-  firstName: string;
-  lastName: string;
+// class Person {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
 
-  /**
-   *
-   */
-  constructor(_id: number, _firsName: string, _lastName: string) {
-    this.id = _id;
-    this.firstName = _firsName;
-    this.lastName = _lastName;
-  }
+//   /**
+//    *
+//    */
+//   constructor(_id: number, _firsName: string, _lastName: string) {
+//     this.id = _id;
+//     this.firstName = _firsName;
+//     this.lastName = _lastName;
+//   }
 
-  getFullname() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
+//   getFullname() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
 
-let kisibilgi = new Person(20, "Zafer", "Önalan");
-console.log(kisibilgi);
-console.log(kisibilgi.getFullname());
+// let kisibilgi = new Person(20, "Zafer", "Önalan");
+// console.log(kisibilgi);
+// console.log(kisibilgi.getFullname());
+
+// ! Public,Privite,Protected
+// ? Public default olarak geliyor. Her yerden erişimi var.
+// ? Protected ise erişimi sadece oluşturlan classı türetildiği yerde erişimi var.
+// ? Private ise erişi hiç bir yerden yok
