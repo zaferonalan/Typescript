e6cca24802b59360ae6d0e19f8728e42685ef61f
zafergunayonalan <zafergunay7@gmail.com>
2023-11-22 00:03:22 +0300

    interface

--- 3-Ders-Class-Interface/app.ts
+++ 3-Ders-Class-Interface/app.ts
@@ -94,30 +94,48 @@
 
 // ! Abstract Class
 
-abstract class Department {
-  constructor(public name: string) {}
+// abstract class Department {
+//   constructor(public name: string) {}
 
-  printName(): void {
-    console.log("Department name: " + this.name);
-  }
+//   printName(): void {
+//     console.log("Department name: " + this.name);
+//   }
 
-  abstract printMeeting(): void;
+//   abstract printMeeting(): void;
+// }
+
+// class AccountingDepartment extends Department {
+//   constructor() {
+//     super("Accounting and Auditing");
+//   }
+//   printMeeting(): void {
+//     console.log("The Accounting Department meets each Monday at 10am.");
+//   }
+
+//   generateReports(): void {
+//     console.log("Generating accounting reports...");
+//   }
+// }
+
+// let department = new AccountingDepartment();
+// department.printName();
+// department.printMeeting();
+// department.generateReports();
+
+// !interface
+
+interface Person {
+  firstName: string;
+  lastName: string;
 }
 
-class AccountingDepartment extends Department {
-  constructor() {
-    super("Accounting and Auditing");
-  }
-  printMeeting(): void {
-    console.log("The Accounting Department meets each Monday at 10am.");
-  }
-
-  generateReports(): void {
-    console.log("Generating accounting reports...");
-  }
+function getFullName(person: Person) {
+  return `${person.firstName} ${person.lastName}`;
 }
 
-let department = new AccountingDepartment();
-department.printName();
-department.printMeeting();
-department.generateReports();
+let person = {
+  firstName: "Zafer Günay",
+  lastName: "Önalan",
+};
+
+console.log(getFullName(person));
