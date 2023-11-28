cd24a1f57021b2e7577cca217e83a22af937d052
zafergunayonalan <zafergunay7@gmail.com>
2023-11-28 22:41:40 +0300

    Generic Interface

--- 4-Ders-Generics/app.js
+++ 4-Ders-Generics/app.js
@@ -2,45 +2,23 @@
 //   let randomIndex = Math.floor(Math.random() * items.length);
 //   return items[randomIndex];
 // }
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
+var List = /** @class */ (function () {
+    function List() {
+        this.items = [];
+    }
+    List.prototype.add = function (o) {
+        this.items.push(o);
+        console.log(this.items);
     };
-    return __assign.apply(this, arguments);
-};
-// let numbers = [1, 54, 65, 7, 8];
-// console.log(getRandomNumber(numbers));
-// function getRandomString(items: string[]): string {
-//   let randomIndex = Math.floor(Math.random() * items.length);
-//   return items[randomIndex];
-// }
-// let strings = ["Zafer", "Merve", "Zerrin"];
-// console.log(getRandomString(strings));
-// ? any kullanımı önerilmemktedir
-// function getRandomElement(items: any[]): any {
-//   let randomIndex = Math.floor(Math.random() * items.length);
-//   return items[randomIndex];
-// }
-// console.log(getRandomElement(strings));
-// console.log(getRandomElement(numbers));
-// ? Generik Kullanılmalı
-// function getRandomElementGeneric<T>(items: T[]): T {
-//   let randomIndex = Math.floor(Math.random() * items.length);
-//   return items[randomIndex];
-// }
-// let degiskenlerim = [true, false, true];
-// console.log(getRandomElementGeneric<number>(numbers));
-// console.log(getRandomElementGeneric<string>(strings));
-// console.log(getRandomElementGeneric<boolean>(degiskenlerim));
-// ! Generic Constraints
-function merge(obj1, obj2) {
-    return __assign(__assign({}, obj1), obj2);
+    List.prototype.remove = function (o) {
+        var index = this.items.indexOf(o);
+        if (index > -1) {
+            this.items.splice(index, 1);
+        }
+    };
+    return List;
+}());
+var list = new List();
+for (var i = 0; i < 10; i++) {
+    list.add(i);
 }
-var person = merge({ name: "Zafer" }, { age: 25 }
-// 25 burada hata vermesi için extends object vererek kısıtladık.
-);
--- 4-Ders-Generics/app.ts
+++ 4-Ders-Generics/app.ts
@@ -36,15 +36,56 @@
 
 // ! Generic Constraints
 
-function merge<U extends object, V extends object>(obj1: U, obj2: V) {
-  return {
-    ...obj1,
-    ...obj2,
-  };
+// function merge<U extends object, V extends object>(obj1: U, obj2: V) {
+//   return {
+//     ...obj1,
+//     ...obj2,
+//   };
+// }
+
+// let person = merge(
+//   { name: "Zafer" },
+//   { age: 25 }
+// 25 burada hata vermesi için extends object vererek kısıtladık.
+// );
+
+// console.log(person);
+
+// ! Generic Interface
+
+// interface Months<U, V> {
+//   key: U;
+//   value: V;
+// }
+
+// let month: Months<number, string> = {
+//   key: 1,
+//   value: "January",
+// };
+
+// console.log(month);
+
+interface Collection<T> {
+  add(o: T): void;
+  remove(o: T): void;
 }
 
-let person = merge(
-  { name: "Zafer" },
-  { age: 25 }
-  // 25 burada hata vermesi için extends object vererek kısıtladık.
-);
+class List<T> implements Collection<T> {
+  private items: T[] = [];
+  add(o: T): void {
+    this.items.push(o);
+    console.log(this.items);
+  }
+  remove(o: T): void {
+    let index = this.items.indexOf(o);
+    if (index > -1) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+
+let list = new List<number>();
+
+for (let i = 0; i < 10; i++) {
+  list.add(i);
+}
